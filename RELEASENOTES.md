8047e1edcb8f26d091685875e9991c40beec9030
Eugene Trusevich <eugene.trusevich@expcapital.com>
2022-06-14 19:16:26 +0300

    feat: new release 1.2.0

--- RELEASENOTES.md
+++ RELEASENOTES.md
@@ -1,5 +1,9 @@
 # Release Notes
 
+### Version 1.2.0 (14/06/2022)
+
+- feat: added new methods, see README.md
+
 ### Version 1.1.0 (10/06/2022)
 
 - feat: added new methods, see index.d.ts
--- www/System.js
+++ www/System.js
@@ -3,82 +3,6 @@
 var SystemPlugin = (function () {
 
 	var exec = require('cordova/exec');
-
-    var mailClients = [
-        {
-            title: 'Spark',
-            scheme: 'readdlespark:'
-        },
-        {
-            title: 'Gmail',
-            scheme: 'googlegmail:'
-        },
-        {
-            title: 'Apple Mail',
-            scheme: 'com.apple.mobilemail:'
-        },
-        {
-            title: 'Outlook',
-            scheme: 'ms-outlook:'
-        },
-        {
-            title: 'Spike',
-            scheme: 'spike:'
-        },
-        {
-            title: 'Airmail',
-            scheme: 'airmail:'
-        },
-        {
-            title: 'Edison Mail',
-            scheme: 'edisonmail:'
-        },
-        {
-            title: 'Twobird',
-            scheme: 'twobird:'
-        },
-        {
-            title: 'Hey',
-            scheme: 'hey:'
-        },
-        {
-            title: 'ProtonMail',
-            scheme: 'protonmail:'
-        },
-        {
-            title: 'Polymail',
-            scheme: 'polymail:'
-        },
-        {
-            title: 'Blue mail',
-            scheme: 'bluemail:'
-        },
-        {
-            title: 'Newton Mail',
-            scheme: 'cloudmagic:'
-        },
-        {
-            title: 'Yandex.Mail',
-            scheme: 'yandexmail:'
-        },
-        {
-            title: 'Mail.ru',
-            scheme: 'mailrumail:'
-        },
-        {
-            title: 'myMail',
-            scheme: 'mycom-mail-x-callback:'
-        },
-        {
-            title: 'Canary Mail',
-            scheme: 'canary:'
-        },
-        {
-            title: 'Yahoo Mail',
-            scheme: 'ymail:'
-        }
-    ];
-
     var pluginName = 'SystemPlugin';
 
 	function SystemPlugin () {}
@@ -104,10 +28,7 @@ var SystemPlugin = (function () {
         },
         getAvailableMailClients: function () {
             return new Promise(function (resolve, reject) {
-                if (!Array.isArray(mailClients)) {
-                    return reject(pluginName + ' mailClients error');
-                }
-                exec(resolve, reject, 'SystemPlugin', 'getAvailableMailClients', [mailClients]);
+                exec(resolve, reject, 'SystemPlugin', 'getAvailableMailClients', []);
             });
         },
 
